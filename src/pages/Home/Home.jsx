74f48a4309422217a4e5fe009162007a16e594e6
Md Belayet Hossain <mdbelayethossain015@gmail.com>
2025-07-03 17:45:12 +0600

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Header from '../shared/Header/Header';
 import Navbar from '../shared/Navbar/Navbar';
+import LeftSideNab from '../shared/LeftSideNab/LeftSideNab';
+import MainNews from '../MainNews/MainNews';
+import RightSideNab from '../shared/RightSideNab/RightSideNab';
 
 const Home = () => {
     return (
@@ -8,6 +11,13 @@ const Home = () => {
             <h2 className="text-3xl font-poppins font-bold">This is home</h2>
             <Header></Header>
             <Navbar></Navbar>
+            <div className='grid grid-cols-3'>
+                <LeftSideNab></LeftSideNab>
+                <div className='col-span-2'>
+                    <MainNews></MainNews>
+                </div>
+                <RightSideNab></RightSideNab>
+            </div>
         </div>
     );
 };
