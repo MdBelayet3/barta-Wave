77036e549e8ff5d66ab9b301de8c1fb31ce89cb5
Md Belayet Hossain <mdbelayethossain015@gmail.com>
2025-07-05 20:12:18 +0600

    Update LeftSideNab.jsx

--- src/pages/shared/LeftSideNab/LeftSideNab.jsx
+++ src/pages/shared/LeftSideNab/LeftSideNab.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router';
 
 const LeftSideNab = () => {
+
+    // useState and useEffect use for loading data
+    const [categories, setCategories] = useState([]);
+
+    useEffect( () =>{
+        fetch('categories.json')
+        .then(res => res.json())
+        .then(data => setCategories(data))
+    },[])
+
     return (
-        <div>
-            <p>This is LeftSide Navbar</p>
+        <div className='space-y-6 sm-p-5'>
+            <p className='text-xl font-semibold'>All Category</p>
+            {
+                categories.map(category => <Link 
+                    className='block hover:bg-[#E7E7E7] py-2 text-xl text-[#9F9F9F] font-semibold  rounded-lg px-10'
+                    to={`/category/${category.id}`}><button className=' hover:text-black'>{category.name}</button></Link>)
+            }
         </div>
     );
 };
