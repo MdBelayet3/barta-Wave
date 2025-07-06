1fcf1d188a9c63610347ca0c01d9dec4e1afdf1c
Md Belayet Hossain <mdbelayethossain015@gmail.com>
2025-07-06 23:30:58 +0600

    Update MainNews.jsx

--- src/pages/MainNews/MainNews.jsx
+++ src/pages/MainNews/MainNews.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AllNews from './AllNews';
 
 const MainNews = () => {
+
+    // useState and useEffect for loading data
+    const [news, setNews] = useState([]);
+
+    useEffect(() => {
+        fetch('news.json')
+            .then(res => res.json())
+            .then(data => setNews(data))
+    }, [])
+
     return (
         <div>
-            <p>News coming Soon .....</p>
+            <p className='text-xl font-semibold'>Dragon News Home : {news.length}</p>
+            <div>
+                {
+                    news.splice(1,2).map(news => <AllNews news={news}></AllNews>)
+                }
+            </div>
         </div>
     );
 };
