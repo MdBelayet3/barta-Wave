cc0173842fbf485417f966f99fb12003916c66c3
Md belayet <mdbelayethossain016@gmail.com>
2025-07-06 17:12:20 +0600

    another file add

--- src/pages/shared/LeftSideNab/NewsCard.jsx
+++ src/pages/shared/LeftSideNab/NewsCard.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import { CiCalendar } from "react-icons/ci";
 
-const NewsCard = () => {
+const NewsCard = ({ news }) => {
+    console.log(news)
     return (
-        <div>
-            
-        </div>
+        <>
+            <a href={`/category/${news?.category_id}`}>
+                <div className="space-y-5 shadow-xl p-3 border-2 rounded-md mt-4">
+                    <img className="rounded-lg" src={news?.image_url} alt="" />
+                    <h2 className="text-[#403F3F] text-xl font-semibold">{news?.title}</h2>
+                    <div className="space-y-3">
+                        <h5 className="font-medium">Category: {news?.category_name}</h5>
+                        <span className="flex items-center gap-4">
+                            <CiCalendar />
+                            <p className="font-medium text-[#9F9F9F]">{news?.author?.published_date}</p>
+                        </span>
+                    </div>
+                </div>
+            </a>
+        </>
     );
 };
 
