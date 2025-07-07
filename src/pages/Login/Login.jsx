1cecfa0ac73af75a5a9166b6cc952057a8101775
Md Belayet Hossain <mdbelayethossain015@gmail.com>
2025-07-07 21:15:33 +0600

    Update Login.jsx

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import Navbar from '../shared/Navbar/Navbar';
 
 const Login = () => {
     return (
         <div>
-            
+            <Navbar></Navbar>
+            <div className="hero mt-10 bg-base-200">
+                <div className="hero-content">
+                    <div className="card bg-base-100 max-w-sm shrink-0 shadow-2xl">
+                        <h2 className='text-4xl font-semibold text-center mt-3'>Please Login</h2>
+                        <form className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Login</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
