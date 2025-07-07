6ae67470a8a2acda246c541f9a1b089381dd6e31
Md Belayet Hossain <mdbelayethossain015@gmail.com>
2025-07-07 21:52:42 +0600

    Update Login.jsx

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import Navbar from '../shared/Navbar/Navbar';
+import { Link } from 'react-router';
 
 const Login = () => {
+    // handle login function
+    const handleLogin = e =>{
+        e.preventDefault();
+        console.log(e.currentTarget);
+        const form = new FormData(e.currentTarget);
+        console.log(form);
+        const email = form.get('email');
+        const password = form.get('password');
+        console.log(email, password);
+    }
+    
     return (
         <div>
             <Navbar></Navbar>
@@ -9,18 +21,18 @@ const Login = () => {
                 <div className="hero-content">
                     <div className="card bg-base-100 max-w-sm shrink-0 shadow-2xl">
                         <h2 className='text-4xl font-semibold text-center mt-3'>Please Login</h2>
-                        <form className="card-body">
+                        <form onSubmit={handleLogin} className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" placeholder="email" className="input input-bordered" required />
+                                <input name='email' type="email" placeholder="email" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                <input name='password' type="password" placeholder="password" className="input input-bordered" required />
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
@@ -28,6 +40,7 @@ const Login = () => {
                             <div className="form-control mt-6">
                                 <button className="btn btn-primary">Login</button>
                             </div>
+                            <p className='font-semibold mt-3'>Do not Have An Account ? <Link to="/register" className='text-blue-500 font-bold'>Register</Link></p>
                         </form>
                     </div>
                 </div>
