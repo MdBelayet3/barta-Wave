f61175eef140903e0ab4cb81ad67591c721dab0b
Md belayet <mdbelayethossain016@gmail.com>
2025-07-07 21:58:21 +0600

    update 4 file

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,9 +1,62 @@
 import React from 'react';
+import Navbar from '../shared/Navbar/Navbar';
+import { Link } from 'react-router';
 
 const Register = () => {
+     // handle login function
+    const handleRegister = e =>{
+        e.preventDefault();
+        console.log(e.currentTarget);
+        const form = new FormData(e.currentTarget);
+        console.log(form);
+        const email = form.get('email');
+        const password = form.get('password');
+        console.log(email, password);
+    }
+
     return (
         <div>
-            
+            <Navbar></Navbar>
+            <div className="hero mt-10 bg-base-200">
+                <div className="hero-content">
+                    <div className="card bg-base-100 max-w-sm shrink-0 shadow-2xl">
+                        <h2 className='text-4xl font-semibold text-center mt-3'>Please Register</h2>
+                        <form onSubmit={handleRegister} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Your Name</span>
+                                </label>
+                                <input name='name' type="text" placeholder="Name" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Photo URL</span>
+                                </label>
+                                <input name='photo' type="text" placeholder="Photo URL" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input name='email' type="email" placeholder="Email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input name='password' type="password" placeholder="Password" className="input input-bordered" required />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Register</button>
+                            </div>
+                            <p className='font-semibold mt-3'>Already Have An Account ? <Link to="/login" className='text-blue-500 font-bold'>Login</Link></p>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
