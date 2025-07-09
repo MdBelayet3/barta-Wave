6085f6ae8f109f55a300e4f47af80e96e5e9425a
Md Belayet Hossain <mdbelayethossain015@gmail.com>
2025-07-09 23:15:34 +0600

    Update News.jsx

--- src/pages/MainNews/News.jsx
+++ src/pages/MainNews/News.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Header from '../shared/Header/Header';
+import RightSideNab from '../shared/RightSideNab/RightSideNab';
 
 const News = () => {
     return (
         <div>
-            
+            <Header></Header>
+            <div className='grid grid-cols-4'>
+                <div className='col-span-3'>
+                    <p>news details</p>
+                    <button  className='btn btn-secondary'>Latest News</button></div>
+                <RightSideNab></RightSideNab>
+            </div>
         </div>
     );
 };
