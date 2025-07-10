ad207a7d8d8294bf16fc0cb5e448da0227776f13
Md Belayet Hossain <mdbelayethossain015@gmail.com>
2025-07-10 21:52:20 +0600

    Update Router.jsx

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -4,6 +4,7 @@ import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
 import News from "../pages/MainNews/News";
+import PrivetRoute from "./PrivetRoute";
 
 const router = createBrowserRouter([
     {
@@ -16,7 +17,7 @@ const router = createBrowserRouter([
             },
             {
                 path: "/news/:id",
-                element: <News></News>,
+                element: <PrivetRoute><News></News></PrivetRoute>,
                 loader: ()=> fetch('/news.json')
             },
             {
