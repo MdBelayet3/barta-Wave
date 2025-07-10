e1d145ecc216aa9af2a8eea8e958fa9b7848827b
Md Belayet Hossain <mdbelayethossain015@gmail.com>
2025-07-10 21:50:02 +0600

    Update PrivetRoute.jsx

--- src/routes/PrivetRoute.jsx
+++ src/routes/PrivetRoute.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../providers/AuthProvider';
+import { Navigate, useLocation } from 'react-router';
 
 const PrivetRoute = ({children}) => {
-    return (
-        <div>
-            
-        </div>
-    );
+
+    const location = useLocation();
+    console.log(location);
+    const { user, loading } = useContext(AuthContext);
+
+    if(loading){
+        return <span className="loading loading-spinner loading-xl"></span>
+    }
+
+    if(user){
+        return children
+    }
+
+    return <Navigate to="/login"></Navigate>
 };
 
 export default PrivetRoute;
