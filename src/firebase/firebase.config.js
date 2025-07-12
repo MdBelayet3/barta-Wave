4ca18c5df7d16a6b071314a855f23cc566df7cab
Md belayet <mdbelayethossain016@gmail.com>
2025-07-12 09:30:59 +0600

    Apply .gitattributes and normalize line endings as well as i have updated my README.md file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,14 +4,15 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+console.log("Insight the firebase config", import.meta.env.VITE_PASS)
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB_iwAcHDJIZMRBMYKn94SleVbeZMPsUR8",
-  authDomain: "barta-news-f6260.firebaseapp.com",
-  projectId: "barta-news-f6260",
-  storageBucket: "barta-news-f6260.firebasestorage.app",
-  messagingSenderId: "798757532542",
-  appId: "1:798757532542:web:1c8ac5284592236c4d792f"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
